fd18e9fd92ec37938671cba5ada8c5ff26a62215
Skylar <skylarblessings5@gmail.com>
2023-07-07 00:25:35 +0300

    added app.py

--- server/app.py
+++ server/app.py
@@ -11,9 +11,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
-migrate = Migrate(app, db)
- 
+migrate = Migrate(app, db) 
 db.init_app(app)
+
+api= Api(app)
+
+class Index(Resource):
+    def get(self):
+        response_dict = {
+            "message":"welcome back"
+        }
+        response = make_response(response_dict, 200)
+        
+        return response
+    
+api.add_resource(Index,"/")
+
+class Authors(Resource):
+    def get(self):
+        author = Author.query.all()
+        print(author)
+        response= make_response(jsonify(author), 200)
+        
+        return response
+    
+api.add_resource(Authors, "/authors"
+)
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
