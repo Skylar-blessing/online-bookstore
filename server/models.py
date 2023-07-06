c1d4714f7397bb84e472bea709aed935036101e7
StevenMo <stevenmolvin@gmail.com>
2023-07-06 18:16:21 +0300

    Validated inputs.

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,15 +9,25 @@ metadata = MetaData(naming_convention={
 
 db=SQLAlchemy(metadata=metadata)
 
+
+
 class Author(db.Model, SerializerMixin):
     __tablename__ = 'authors'
     id= db.Column('id', db.Integer(), primary_key=True)
     name= db.Column('name', db.String())
     books= db.relationship('Book', backref='author')
     # categories = db.relationship('Author_Category', backref='author')
+    
+    @validates('name')
+    def validate_name(self, key, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string.")
+        return value
 
     def __repr__(self):
         return f"Author('{self.id}', '{self.name}')"
+    
+    
 
 class Book(db.Model, SerializerMixin):
     __tablename__='books'
@@ -26,6 +37,12 @@ class Book(db.Model, SerializerMixin):
     available_copies = db.Column(db.Integer())
     author_id = db.Column(db.Integer(), db.ForeignKey('authors.id'))
     category_id = db.Column(db.Integer(), db.ForeignKey('categories.id'))   
+    
+    @validates('available_copies')
+    def validate_available_copies(self, key, value):
+        if not isinstance(value, int):
+            raise ValueError("Value must be an integer.")
+        return value
     
     def __repr__(self):
         return f"Book('{self.title}')"
